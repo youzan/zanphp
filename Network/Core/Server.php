8cd053c4d0ba8a934f6bf074c4c787d1ba4bfed4
winglechen <winglechen@gmail.com>
2015-10-18 13:42:49 +0800

    add server interface

---
+++ Network/Core/Server.php
@@ -0,0 +1,13 @@
+<?php
+namespace Zanphp\Zan\Network\Core;
+
+interface Server {
+    function onStart($server, $workerId); 
+    function onConnect($server, $clientId, $fromId);
+    function onReceive($server, $clientId, $fromId, $data);
+    function onClose($server, $clientId, $fromId);
+    function onShutdown($server, $workerId);
+    function onTask($server, $taskId, $fromId, $data);
+    function onFinish($server, $taskId, $data);
+    function onTimer($server, $interval);
+}
