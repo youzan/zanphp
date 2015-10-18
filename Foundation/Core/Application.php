3808c926b8432d412eb1d2b3393335029d6172a4
winglechen <winglechen@gmail.com>
2015-10-18 10:22:38 +0800

    add application

--- Foundation/Core/Application.php
+++ Foundation/Core/Application.php
@@ -1,1 +1,1 @@
-<?phpnamespace Zanphp\Zan\Foundation\Core;class Application {        public function init() {            }    }
+<?phpnamespace Zanphp\Zan\Foundation\Core;class Application {    public function __construct() {            }        public function setRootPath($dir=null) {            }    public function setResourcePath($dir=null) {            }    public function setAppName($name) {            }       public function init() {            }    }
