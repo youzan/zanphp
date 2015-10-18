854de6926efda6b9f5cdfca0daf49ea39731d1f9
winglechen <winglechen@gmail.com>
2015-10-18 12:47:53 +0800

    add class alias

---
+++ ClassAlias.php
@@ -0,0 +1,10 @@
+<?php
+
+$alias = [
+    'Config'        => '',
+    'Cache'         => '',
+    'Db'            => '',
+    'Kv'            => '',
+    'Queue'         => '',
+    'HttpClient'    => '',
+];
