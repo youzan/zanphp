c5c5a180b74924e6a536db3c62754b30b3722148
winglechen <winglechen@gmail.com>
2015-10-29 17:32:14 +0800

    add Table method

--- src/Extend/Table.php
+++ src/Extend/Table.php
@@ -1,1 +1,21 @@
 <?php
+
+class Table {
+    const UP    = 1;
+    const DOWN  = 2;
+    const LEFT  = 3;
+    const RIGHT = 4; 
+
+    public static function Join(array $arr1, array $arr2, $direction=Table::RIGHT) {
+        
+    }    
+
+    public static function pluck(array $arr, $columnName) {
+        
+    }
+
+    public static function mapKey(array $arr, $columnName) {
+        
+    }
+}
+
