ecd103890f3aaa99110c38d61dd3a935bf233acc
winglechen <winglechen@gmail.com>
2015-11-01 00:20:04 +0800

    add httpApplication

---
+++ src/Foundation/Exception/ZanException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: winglechen
+ * Date: 15/11/1
+ * Time: 00:18
+ */
+
+namespace Zan\Framework\Foundation\Exception;
+
+
+class ZanException extends \Exception {
+
+}
---
+++ src/Network/Http/Application.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: winglechen
+ * Date: 15/10/31
+ * Time: 23:55
+ */
+
+
+
+class Application extends \Zan\Framework\Network\Contract\Application {
+    public function init() {
+        parent::init();
+    }
+}
