9810236e0f5002c21a7d9c9391cd14ee31f24874
winglechen <winglechen@gmail.com>
2015-11-01 22:22:32 +0800

    add Event

---
+++ src/Foundation/Core/Event.php
@@ -0,0 +1,17 @@
+<?php
+
+class Event {
+    private static $evtMap = [];
+
+    public static function register($evtName) {
+        
+    }
+
+    public static function bind($evtName, callable $callback) {
+        
+    }
+
+    public static function fire($evtName) {
+        
+    }
+}
