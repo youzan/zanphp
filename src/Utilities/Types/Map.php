97de5caa33382d0ec1f9c941b80e914f107a6771
winglechen <winglechen@gmail.com>
2015-12-03 18:09:56 +0800

    add Map and Money

--- src/Utilities/Types/Map.php
+++ src/Utilities/Types/Map.php
@@ -4,5 +4,23 @@ namespace Zan\Framework\Utilities\Types;
 class Map {
     private $data = null;
 
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    public function get($key, $default=null)
+    {
+        if(!isset($this->data[$key])) {
+            return $default;
+        }
+
+        return $this->data[$key];
+    }
+
+    public function set($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
 
 }
--- src/Utilities/Types/Money.php
+++ src/Utilities/Types/Money.php
@@ -1,1 +1,21 @@
 <?php
+namespace Zan\Framework\Utilities\Types;
+
+
+class Money {
+    private $num = 0;
+    public function __construct($num)
+    {
+        $this->num = $num;
+    }
+
+    public function Rmb()
+    {
+
+    }
+
+    public function Dollar()
+    {
+
+    }
+}
