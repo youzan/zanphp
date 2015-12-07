73a81f88255e6543a7fefb6bf70cb057794f6f7d
winglechen <winglechen@gmail.com>
2015-12-07 20:01:11 +0800

    load Commands For Task

--- src/Foundation/Coroutine/Commands.php
+++ src/Foundation/Coroutine/Commands.php
@@ -7,11 +7,21 @@
  */
 namespace Zan\Framework\Foundation\Coroutine;
 
+use Zan\Framework\Utilities\Types\Dir;
+
 class Commands
 {
     public static function load()
     {
+        $dir = __DIR__ . '/Command/';
+        $files = Dir::glob($dir, '*.php');
 
+        if(!$files) return false;
+        
+        foreach($files as $file) {
+            require $file;
+        }
+        return true;
     }
 }
 
