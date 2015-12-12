19b6a9c31cdcbd9ea59ffb60dbe035d9bf5e0c29
winglechen <winglechen@gmail.com>
2015-12-12 22:27:05 +0800

    add some dirs

---
+++ src/Network/Facade/Rpc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: winglechen
+ * Date: 15/12/12
+ * Time: 22:20
+ */
+
+namespace Zan\Framework\Network\Facade;
+
+
+class Rpc {
+    public static function call($api, $parameter=[], callable $callbace=null)
+    {
+
+    }
+}
---
+++ src/Store/Facade/Cache.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: winglechen
+ * Date: 15/12/12
+ * Time: 22:23
+ */
+namespace Zan\Framework\Store;
+
+class Cache {
+    public function get(){}
+    public function set(){}
+}
