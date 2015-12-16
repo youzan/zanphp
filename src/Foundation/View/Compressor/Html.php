59781bff3be7dae534351a6e5ad93e2951332efa
winglechen <winglechen@gmail.com>
2015-12-16 21:59:29 +0800

    add view compressor

---
+++ src/Foundation/View/Compressor/Html.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: winglechen
+ * Date: 15/12/16
+ * Time: 21:57
+ */
+
+namespace Zan\Framework\Foundation\View\Compressor;
+
+
+class Html {
+    public static function compress($html)
+    {
+        return $html;
+    }
+}
