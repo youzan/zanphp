7f91cde11281014ecabbd6b48347cf6d95c6dd1d
winglechen <winglechen@gmail.com>
2016-02-29 20:06:50 +0800

    add Arr::sortByArray test case

--- src/Utilities/Types/Arr.php
+++ src/Utilities/Types/Arr.php
@@ -17,4 +17,36 @@ class Arr {
 
         return $before;
     }
+
+    public static function sortByArray(array $arr, array $sort, $withNotExists=false)
+    {
+        if(!$sort) return $arr;
+        if(!$arr) return [];
+
+        $ret = [];
+        $notExist = [];
+        $map = array_flip($arr);
+
+        foreach($sort as $item){
+            if(isset($map[$item])){
+                $ret[] = $item;
+                unset($map[$item]);
+            }else{
+                $notExist[] = $item;
+            }
+        }
+
+        if(!empty($map)){
+            $ret = Arr::join($ret, array_keys($map));
+        }
+
+        if(false === $withNotExists){
+            return $ret;
+        }
+
+        return [
+            'result' => $ret,
+            'notExist' => $notExist
+        ];
+    }
 }
