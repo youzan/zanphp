edadff9ab14ec974b93befc54a86a7bdc4e82a40
xiaolong <xiaolong@qima-inc.com>
2016-03-01 17:54:54 +0800

    属性‘

--- src/Network/Contract/ConnectionPool.php
+++ src/Network/Contract/ConnectionPool.php
@@ -21,4 +21,17 @@ class ConnectionPool extends ObjectPool{
     {
 
     }
+
+    public function configDemo() {
+        $config = new Config();
+        $config->set('host', '192.168.66.202:3306');
+        $config->set('user', 'test_koudaitong');
+        $config->set('pool_name', 'p_zan');
+        $config->set('maximum-connection-count', '100');
+        $config->set('minimum-connection-count', '10');
+        $config->set('keeping-sleep-time', '90000');
+        $config->set('maximum-new-connections', '5');
+        $config->set('prototype-count', '5');
+
+    }
 }
