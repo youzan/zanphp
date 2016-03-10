dc3750f1b55d71086773e3d3cf7f84698b1d573d
funny <funny@qima-inc.com>
2016-03-10 19:57:42 +0800

    add view

--- src/Foundation/View/View.php
+++ src/Foundation/View/View.php
@@ -1,8 +1,40 @@
 <?php
 namespace Zan\Framework\Foundation\View;
 
+use Zan\Framework\Foundation\View\Layout;
+use Zan\Framework\Foundation\View\Js;
+use Zan\Framework\Foundation\View\Css;
+use Zan\Framework\Foundation\View\Tpl;
+use Zan\Framework\Foundation\View\Form;
+
 class View
 {
+    private $_layout = null;
+    private $_js = null;
+    private $_css = null;
+    private $_tpl = null;
+    private $_form = null;
+
+    private $_data = [];
+
+    public function __construct($tpl, array $data)
+    {
+        $this->_layout = new Layout($tpl, $data);
+        $this->_js = new Js();
+        $this->_css = new Css();
+        $this->_tpl = new Tpl();
+        $this->_form = new Form();
+
+        $this->_data = $data;
+    }
+
+    public static function display($tpl, $data)
+    {
+        $view = new self($tpl,$data);
+
+        return trim($view->render(), " \r\n");
+    }
+
     public function render()
     {
         extract($this->getViewVars());
@@ -12,10 +44,11 @@ class View
     {
         return [
             'view' => $this,
-            'layout' => new Layout(),
-            'form' => new Form(),
-            'js' => new Js(),
-            'css' => new Css(),
+            'layout' => $this->_layout,
+            'form' => $this->_form,
+            'js' => $this->_js,
+            'css' => $this->_css,
+            'tpl' => $this->_tpl
         ];
     }
 }
