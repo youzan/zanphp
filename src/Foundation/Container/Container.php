f7d447797642d059c40daa8192f038cd24532f0a
winglechen <winglechen@gmail.com>
2016-03-16 16:26:57 +0800

    Di

---
+++ src/Foundation/Container/Container.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Zan\Framework\Foundation\Container\Container;
+
+use ReflectionClass;
+use Zan\Framework\Utilities\DesignPattern\Singleton;
+
+class Container
+{
+    use Singleton;
+
+    protected $instances = [];
+
+    private function __construct()
+    {
+
+    }
+
+    public function get($abstract)
+    {
+        $abstract = $this->normalize($abstract);
+
+        if (isset($this->instances[$abstract])) {
+            return $this->instances[$abstract];
+        }
+
+        return null;
+    }
+
+    public function set($alias, $instance)
+    {
+        if (!isset($this->instances[$alias])) {
+            $this->instances[$alias] = $instance;
+        }
+    }
+
+    public function make($abstract, array $parameters = [], $shared = false)
+    {
+        $abstract = $this->normalize($abstract);
+
+        if ($shared && isset($this->instances[$abstract])) {
+            return $this->instances[$abstract];
+        }
+
+        $class = new ReflectionClass($abstract);
+        $object = $class->newInstanceArgs($parameters);
+
+        if ($shared && $object !== null) {
+            $this->instances[$abstract] = $object;
+        }
+
+        return $object;
+    }
+
+    /**
+     * Normalize the given class name by removing leading slashes.
+     *
+     * @param  string $className
+     * @return string
+     */
+    protected function normalize($className)
+    {
+        return is_string($className) ? ltrim($className, '\\') : $className;
+    }
+}
