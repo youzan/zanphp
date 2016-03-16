a659bac10b0e28cbed813506bc9ec840fdd23085
卫星 <voidman.me@gmail.com>
2016-03-16 16:53:27 +0800

    fix namespace

--- .zan-ide-helper.php
+++ .zan-ide-helper.php
@@ -1,3 +1,3 @@
 <?php
-class UnitTest extends \Zan\Framework\Foundation\Test\UnitTest {}
+class UnitTest extends \Zan\Framework\Testing\UnitTest {}
 class TcpServer extends \Zan\Framework\Network\Tcp\Server {}
