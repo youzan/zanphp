0c9e73cda66f30b53ae2d66108e06ffce748ba14
nuomi <agalwood.net@gmail.com>
2016-03-31 15:08:54 +0800

    code format

--- src/Testing/TaskTest.php
+++ src/Testing/TaskTest.php
@@ -1,26 +1,28 @@
 <?php
 /**
- *  @see zan/test/Foundation/Coroutine/TaskDemoTest.php
+ * @see zan/test/Foundation/Coroutine/TaskDemoTest.php
  */
-namespace Zan\Framework\Testing;;
+namespace Zan\Framework\Testing;
 
 use Zan\Framework\Foundation\Core\Event;
 use Zan\Framework\Foundation\Core\EventChain;
 use Zan\Framework\Foundation\Coroutine\Task;
 use Zan\Framework\Test\Foundation\Coroutine\Task\parallel;
 
-class TaskTest extends UnitTest {
+class TaskTest extends UnitTest
+{
     protected $taskMethodPattern = '/^task.+/i';
 
     protected $isInitialized = false;
     protected $counter = 0;
     protected $coroutines = [];
 
-    public function init() {
+    public function init()
+    {
         if ($this->isInitialized) {
             return false;
         }
-        Event::bind('test_task_done', function() {
+        Event::bind('test_task_done', function () {
             swoole_event_exit();
         });
         $this->isInitialized = true;
@@ -34,9 +36,9 @@ class TaskTest extends UnitTest {
         $ref = new \ReflectionClass($this);
         $methods = $ref->getMethods(\ReflectionMethod::IS_PROTECTED | \ReflectionMethod::IS_PUBLIC);
 
-        foreach($methods as $method) {
+        foreach ($methods as $method) {
             $methodName = $method->getName();
-            if(!preg_match($this->taskMethodPattern, $methodName)){
+            if (!preg_match($this->taskMethodPattern, $methodName)) {
                 continue;
             }
 
