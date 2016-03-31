dcfd2569a30dab33d253c5caddb3368adc1de16e
nuomi <agalwood.net@gmail.com>
2016-03-31 15:06:32 +0800

    test add events

--- src/Testing/TaskTest.php
+++ src/Testing/TaskTest.php
@@ -4,13 +4,33 @@
  */
 namespace Zan\Framework\Testing;;
 
+use Zan\Framework\Foundation\Core\Event;
+use Zan\Framework\Foundation\Core\EventChain;
 use Zan\Framework\Foundation\Coroutine\Task;
+use Zan\Framework\Test\Foundation\Coroutine\Task\parallel;
 
 class TaskTest extends UnitTest {
     protected $taskMethodPattern = '/^task.+/i';
 
+    protected $isInitialized = false;
+    protected $counter = 0;
+    protected $coroutines = [];
+
+    public function init() {
+        if ($this->isInitialized) {
+            return false;
+        }
+        Event::bind('test_task_done', function() {
+            swoole_event_exit();
+        });
+        $this->isInitialized = true;
+    }
+
     public function testTasksWork()
     {
+        $this->counter++;
+        EventChain::before('test_task_num_' . $this->counter, 'test_task_done');
+
         $ref = new \ReflectionClass($this);
         $methods = $ref->getMethods(\ReflectionMethod::IS_PROTECTED | \ReflectionMethod::IS_PUBLIC);
 
@@ -21,10 +41,15 @@ class TaskTest extends UnitTest {
             }
 
             $coroutine = $this->$methodName();
-            Task::execute($coroutine);
+            $this->coroutines[] = $coroutine;
         }
     }
 
+    public function runTest()
+    {
+        yield parallel($this->coroutines);
+        Event::fire('test_task_done');
+    }
 
 }
 
