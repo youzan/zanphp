a9a7e29804385b774f6ebcbdc6515e5a59faa7cc
nuomi <agalwood.net@gmail.com>
2016-03-31 18:26:17 +0800

    Cookie get

--- src/Network/Http/Cookie.php
+++ src/Network/Http/Cookie.php
@@ -11,23 +11,39 @@ namespace Zan\Framework\Network\Http;
 use Zan\Framework\Foundation\Core\Config;
 use Zan\Framework\Foundation\Exception\System\InvalidArgumentException;
 use Zan\Framework\Network\Http\Request\Request;
-
+use swoole_http_response as SwooleHttpResponse;
 
 class Cookie
 {
     private $configKey = 'cookie';
+    private $request;
+    private $response;
 
-    public function __construct(Request $request, $swooleResponse)
+    public function __construct(Request $request, SwooleHttpResponse $swooleResponse)
     {
-        $this->init();
+        $this->init($request, $swooleResponse);
     }
 
-    private function init()
+    private function init(Request $request, SwooleHttpResponse $swooleResponse)
     {
         $config = Config::get($this->configKey, null);
         if (!$config) {
             throw new InvalidArgumentException('cookie config is required');
         }
+        $this->request = $request;
+        $this->response = $swooleResponse;
+    }
+
+    public function get($name, $default = '')
+    {
+        if (!$name) {
+            yield '';
+        }
+
+        if (!isset($this->request->cookies[$name])) {
+            yield $default;
+        }
+        yield $this->request->cookies[$name];
     }
 
     public function set($name, $value = null, $expire = null, $path = null, $domain = null, $secure = null, $httponly = null)
@@ -35,8 +51,4 @@ class Cookie
 
     }
 
-    public static function get()
-    {
-
-    }
 }
