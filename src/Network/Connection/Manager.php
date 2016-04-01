2383ad7b9dc08aaf8f6a3c38b06b2f506be1745e
winglechen <winglechen@gmail.com>
2016-04-01 23:11:33 +0800

    add connection manager

---
+++ src/Network/Connection/Manager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: winglechen
+ * Date: 16/4/1
+ * Time: 18:12
+ */
+
+namespace Zan\Framework\Network\Connection;
+
+
+class Manager
+{
+    public static function init()
+    {
+        
+    }
+
+    /**
+     * @param $connKey
+     * @return 
+     */
+    public static function get($connKey)
+    {
+        
+    }
+}
