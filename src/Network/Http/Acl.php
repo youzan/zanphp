fda8a600d6fd041a18102409d330801df0bfc417
nuomi <agalwood.net@gmail.com>
2016-04-07 15:11:41 +0800

    acl fix

--- src/Network/Http/Acl.php
+++ src/Network/Http/Acl.php
@@ -11,28 +11,24 @@ namespace Zan\Framework\Network\Http;
 use Zan\Framework\Foundation\Core\Config;
 use Zan\Framework\Network\Http\Request\Request;
 use Zan\Framework\Network\Http\Response\RedirectResponse;
-use Zan\Framework\Utilities\DesignPattern\Context;
 
 class Acl
 {
     private $configKey = 'acl';
 
-    public function __construct(Request $request, Context $context)
+    public function __construct(Request $request)
     {
         $this->request = $request;
-        $this->context = $context;
         $this->config = Config::get($this->configKey, null);
     }
 
     public function auth()
     {
         $cookie = (yield getCookieHandler());
-        $sid = $cookie->get('sid');
-        if (!$sid) {
+        $sid = $cookie->get('sid', '');
+        if ('' === $sid) {
+            $cookie->set('redirect', $this->request->fullUrl());
             yield RedirectResponse::create($this->config['login_url'], 302);
-        } else {
-            $this->context->set('sid', $sid);
-            yield null;
         }
     }
 
