125ee042d648cdc3d4eaf0713629924f6289e5e0
funny <funny@qima-inc.com>
2016-04-07 11:17:24 +0800

    improve

--- test/Network/Http/Routing/routing_new/goods.php
+++ test/Network/Http/Routing/routing_new/goods.php
@@ -7,7 +7,7 @@
  */
 return [
     'goods/:payType/:kdt_id/xxx' => [
-        'rewrite' => 'goods',
+        'rewrite' => 'goods/${payType}',
         'example' => [
             'goods/wxpay/xxx' => 'goods/wxpay/index',
             'goods/alipay/xxx' => 'goods/alipay/index',
