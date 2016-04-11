b33da85b60aa23340b7babe69dbc2faa476bc7e8
nuomi <agalwood.net@gmail.com>
2016-04-11 18:19:14 +0800

    acl add white list

--- src/Network/Http/Acl.php
+++ src/Network/Http/Acl.php
@@ -12,6 +12,7 @@ use Zan\Framework\Foundation\Core\Config;
 use Zan\Framework\Network\Http\Request\Request;
 use Zan\Framework\Network\Http\Response\RedirectResponse;
 use Zan\Framework\Network\Common\Client;
+use Zan\Framework\Utilities\Types\Arr;
 
 class Acl
 {
@@ -25,6 +26,11 @@ class Acl
 
     public function auth()
     {
+        $aclWhiteList = $this->config['white_list'];
+        $currentPath = $this->request->getPath();
+        if (in_array($currentPath, $aclWhiteList)) {
+            return;
+        }
         $cookie = (yield getCookieHandler());
         $sid = (yield $cookie->get('sid', ''));
         $userId = (yield $cookie->get('user_id', 0));
@@ -34,17 +40,44 @@ class Acl
             return;
         } else {
             if (0 === $userId) {
-                $userId = (yield $this->getAdminIdBySid($sid));
-                $cookie->set('user_id', $userId, 0);
+                yield $this->setAdminInfoToCookie($sid);
             }
         }
 
         yield null;
     }
 
+    private function setAdminInfoToCookie($sid)
+    {
+        if (!$sid) {
+            return;
+        }
+        $cookie = (yield getCookieHandler());
+        $userId = (yield $this->getAdminIdBySid($sid));
+        yield $cookie->set('user_id', $userId, 0);
+
+        $adminInfo = (yield $this->getAdminInfoById($userId));
+        if (isset($adminInfo['account'])) {
+            yield $cookie->set('account', $adminInfo['account'], 0);
+        }
+        if (isset($adminInfo['avatar'])) {
+            yield $cookie->set('avatar', $adminInfo['avatar'], 0);
+        }
+        if (isset($adminInfo['nickname'])) {
+            yield $cookie->set('nickname', $adminInfo['nick_name'], 0);
+        }
+    }
+
     private function getAdminIdBySid($sid)
     {
         $resp = (yield Client::call('account.sso.getAdminIdBySid', ['sid' => $sid]));
+        $result = $resp['code'] === 0 ? $resp['data'] : null;
+        yield $result;
+    }
+
+    private function getAdminInfoById($id)
+    {
+        $resp = (yield Client::call('account.admin.getPersonalInfo', ['admin_id' => $id]));
         $result = $resp['code'] === 0 ? $resp['data'] : null;
         yield $result;
     }
