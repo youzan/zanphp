882061b36a13d5b3c982ec1a14359a5795b8487f
Xxvholic <xxvholic.wu@gmail.com>
2016-04-11 10:55:22 +0800

    http client

--- src/Network/Common/Client.php
+++ src/Network/Common/Client.php
@@ -1,19 +1,58 @@
 <?php
 
-/**
- * @author hupp
- * create date: 16/03/02
- */
-
 namespace Zan\Framework\Network\Common;
 
-use Zan\Framework\Network\Http\Client as HttpClient;
+use Zan\Framework\Network\Http\Client\HttpClient;
+use Zan\Framework\Foundation\Contract\Async;
 
-class Client
+class Client implements Async
 {
-    public static function call($path, $parameter = [], $method = 'POST')
+    /** @var  HttpClient */
+    private $httpClient;
+
+    public static function call($api, $parameter = [], $method = 'POST')
     {
-        yield (new HttpClient($path, $parameter, $method));
+        $apiConfig = ClientRouter::lookup($api);
+
+        $client = new self($apiConfig['host'], $apiConfig['port']);
+        $client->setApi($api);
+        $client->setMethod($method);
+        $client->setTimeout($apiConfig['timeout']);
+        $client->setParams($parameter);
+
+        yield $client;
     }
 
+    private function __construct($host, $port)
+    {
+        $this->httpClient = new HttpClient($host, $port);
+    }
+
+    private function setApi($api)
+    {
+        if (false != strpos($api, '.')) {
+             $this->httpClient->setUri('/' . str_replace('.', '/', $api));
+        }
+    }
+
+    private function setMethod($method)
+    {
+        $this->httpClient->setMethod($method);
+    }
+
+    private function setTimeout($timeout)
+    {
+        $this->httpClient->setTimeout($timeout);
+    }
+
+    private function setParams($params)
+    {
+        $params['debug'] = 'json';
+        $this->httpClient->setParams($params);
+    }
+
+    public function execute(Callable $callback)
+    {
+        $this->httpClient->setCallback($callback)->handle();
+    }
 }
---
+++ test/Network/Http/config/dev/services.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'php' => [
+        'host' => 'api.koudaitong.com',
+        'port' => 80,
+        'timeout' => 2
+    ],
+    'java' => [
+        'account.team'
+    ],
+
+];
