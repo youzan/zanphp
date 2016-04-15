190e044f7e1aecc9f750e8773b65e7a1d2aa5091
funny <funny@qima-inc.com>
2016-04-15 22:28:43 +0800

    add function for read php run mode from php.ini

--- src/Foundation/Booting/InitializeRunMode.php
+++ src/Foundation/Booting/InitializeRunMode.php
@@ -13,9 +13,22 @@ use Zan\Framework\Contract\Foundation\Bootable;
 use Zan\Framework\Foundation\Application;
 use Zan\Framework\Foundation\Core\RunMode;
 
-class InitializeRunMode implements Bootable{
+class InitializeRunMode implements Bootable
+{
+    private $runModeKey = 'RUN_MODE';
+
     public function bootstrap(Application $app)
     {
-        RunMode::set('dev');
+        RunMode::set($this->getRunMode());
+    }
+
+    private function getRunMode(){
+        return 'dev';
+        $key = 'kdt.' . $this->runModeKey;
+        $value = get_cfg_var($key);
+        if(!$value && isset($_SERVER[$key])){
+            $value = $_SERVER[$key];
+        }
+        return $value;
     }
 }
