3055ac8981321642ddec6d835d26ef808ce6ea41
nuomi <agalwood.net@gmail.com>
2016-04-15 11:30:33 +0800

    zan exception

--- src/Foundation/Exception/ExceptionHandlerChain.php
+++ src/Foundation/Exception/ExceptionHandlerChain.php
@@ -8,7 +8,6 @@
 
 namespace Zan\Framework\Foundation\Exception;
 
-
 use Zan\Framework\Contract\Foundation\ExceptionHandler;
 use Zan\Framework\Foundation\Exception\Handler\ExceptionLogger;
 
@@ -18,12 +17,12 @@ class ExceptionHandlerChain
         ExceptionLogger::class,
     ];
     protected $handlerMap = [];
-    
+
     public function __construct()
     {
         $this->clearHandlers();
     }
-    
+
     public function clearHandlers()
     {
         $this->handlerChain = [];
@@ -32,55 +31,55 @@ class ExceptionHandlerChain
 
     public function handle(\Exception $e)
     {
-        if(empty($this->handlerChain)){
+        if (empty($this->handlerChain)) {
             throw $e;
         }
 
         //at less one handler handle the exception
         //else throw the exception out
         $exceptionHandled = false;
-        foreach ($this->handlerChain as $handler){
+        foreach ($this->handlerChain as $handler) {
             $status = $handler->handle($e);
-            if(true === $status){
+            if (true === $status) {
                 $exceptionHandled = true;
             }
         }
-        
-        if(false === $exceptionHandled){
+
+        if (false === $exceptionHandled) {
             throw $e;
         }
     }
-    
+
     public function addHandler(ExceptionHandler $handler)
     {
         $hash = spl_object_hash($handler);
-        if(isset($this->handlerMap[$hash])){
+        if (isset($this->handlerMap[$hash])) {
             return false;
         }
 
-        $this->handlerMap[$hash]= true;
+        $this->handlerMap[$hash] = true;
         $this->handlerChain[] = $handler;
     }
-    
+
     public function addHandlerByName($handlerName)
     {
-        if(isset($this->handlerMap[$handlerName])){
+        if (isset($this->handlerMap[$handlerName])) {
             return false;
         }
 
-        $this->handlerMap[$handlerName]= true;
+        $this->handlerMap[$handlerName] = true;
         $this->handlerChain[] = new $handlerName();
     }
-    
+
     public function addHandlersByName(array $handlers)
     {
-        if(!$handlers){
+        if (!$handlers) {
             return false;
-        }    
-        
-        foreach ($handlers as $handlerName){
+        }
+
+        foreach ($handlers as $handlerName) {
             $this->addHandlerByName($handlerName);
         }
     }
-    
+
 }
