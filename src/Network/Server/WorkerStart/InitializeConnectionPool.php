ec150400d96ec605e8c14748cb2c686dd0e71735
小牛 <xiaoniu@qima-inc.com>
2016-04-16 15:32:08 +0800

    sql map

--- src/Network/Server/WorkerStart/InitializeConnectionPool.php
+++ src/Network/Server/WorkerStart/InitializeConnectionPool.php
@@ -2,11 +2,11 @@
 
 namespace Zan\Framework\Network\Server\WorkerStart;
 
-use Zan\Framework\Network\Tcp\Server;
 use Zan\Framework\Foundation\Core\Config;
 use Zan\Framework\Network\Connection\ConnectionInitiator;
-
-class InitializeConnectionPool
+use Zan\Framework\Contract\Network\Bootable;
+use Zan\Framework\Contract\Network\Server;
+class InitializeConnectionPool implements Bootable
 {
     /**
      * @param
