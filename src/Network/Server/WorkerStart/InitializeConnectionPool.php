595c9e7db9c5d230e6d60e58d6c1e7f8bfdcaef9
小牛 <xiaoniu@qima-inc.com>
2016-04-16 14:46:16 +0800

    connection

--- src/Network/Server/WorkerStart/InitializeConnectionPool.php
+++ src/Network/Server/WorkerStart/InitializeConnectionPool.php
@@ -2,10 +2,18 @@
 
 namespace Zan\Framework\Network\Server\WorkerStart;
 
+use Zan\Framework\Network\Tcp\Server;
+use Zan\Framework\Foundation\Core\Config;
+use Zan\Framework\Network\Connection\ConnectionInitiator;
+
 class InitializeConnectionPool
 {
-    public function bootstrap($server, $workerId)
+    /**
+     * @param
+     */
+    public function bootstrap(Server $server)
     {
-        
+        $config = Config::get('connection');
+        ConnectionInitiator::getInstance()->init($config);
     }
 }
