0e104fc7e7d6c02a0869c831ec92a8b449698e8c
suqiangithub <aoxue.1988.su.qian@163.com>
2016-04-18 19:55:06 +0800

    short url

---
+++ src/Utilities/Types/ShortUrl.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suqian
+ * Date: 16/4/18
+ * Time: 下午7:40
+ */
+
+namespace Zan\Framework\Utilities\Types;
+
+
+use Zan\Framework\Foundation\Core\Config;
+use Zan\Framework\Foundation\Exception\System\InvalidArgumentException;
+use Zan\Framework\Network\Common\HttpClient;
+
+class ShortUrl
+{
+    public static function get($url){
+        if (!trim($url)){
+            throw new InvalidArgumentException('链接地址错误');
+        }
+        $config = Config::get('shorturl');
+        $response = (yield HttpClient::newInstance($config['host'],$config['port'])->get('/shorten?longUrl='.$url));
+        $data = json_decode($response,true);
+        if(!isset($data['status_code']) || !isset($data['data'])){
+            yield '';
+            return;
+        }
+
+        if(200 == $data['status_code'] && isset($data['data']) && isset($data['data']['url'])){
+            yield $data['data']['url'];
+            return;
+        }
+        yield '';
+    }
+
+    private static function format($url,$failReturnPre=false)
+    {
+        if(false === $failReturnPre){
+            return '';
+        }
+        return $url;
+    }
+
+}
