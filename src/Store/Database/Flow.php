3099183d220644c35958514d42fcb6f43661dd23
小牛 <xiaoniu@qima-inc.com>
2016-04-18 16:10:28 +0800

    sql map

--- src/Store/Database/Flow.php
+++ src/Store/Database/Flow.php
@@ -21,7 +21,7 @@ class Flow
         $connection = (yield ConnectionManager::getInstance()->get($database));
         $mysql = new Mysql($connection);
         $dbResult = (yield $mysql->query($sqlMap['sql']));
-        $resultFormatter = new ResultFormatter($dbResult);
+        $resultFormatter = new ResultFormatter($dbResult, $sqlMap['result_type']);
         yield $resultFormatter->format();
         return;
     }
