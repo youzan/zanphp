36f3047b32b2c1a80c22627e2d2db6d83d2569de
小牛 <xiaoniu@qima-inc.com>
2016-05-05 17:10:31 +0800

    sql map

--- test/Store/Database/MysqlTest.php
+++ test/Store/Database/MysqlTest.php
@@ -8,70 +8,73 @@
 
 
 namespace Zan\Framework\Test\Store\Database;
-
-use Zan\Framework\Test\Foundation\Coroutine\Context;
-use Zan\Framework\Foundation\Coroutine\Task;
-use Zan\Framework\Test\Store\Database\Sql\Task\InsertJob;
-use Zan\Framework\Test\Store\Database\Sql\Task\SelectJob;
-use Zan\Framework\Test\Store\Database\Sql\Task\UpdateJob;
-use Zan\Framework\Store\Database\Mysql\QueryResult;
-class MysqlTest extends \TestCase
+use Zan\Framework\Testing\TaskTest;
+use Zan\Framework\Store\Facade\Db;
+class MysqlTest extends TaskTest
 {
-/*
+
     public function testInsert()
     {
-        $context = new Context();
-        $job = new InsertJob($context);
-        $sid = 'demo.insert';
-        $data = [
-            'insert' => [
-                'name' => "''a'''''",
-                'nick_name' => 'b',
-                'id_number' => '330323198888888888',
-                'gender' => 0
-            ]
-        ];
-        $options = [];
-        $job->setSid($sid)->setInsert($data)->setOptions($options);
-        $coroutine = $job->run();
-
-        $task = new Task($coroutine);
-        $task->run();
-        $result = $context->show();
-        $this->assertGreaterThan(0, $result['response']);
+        $sid = '';
+        $data = [];
+        $data = (yield Db::execute());
+        $this->assertGreaterThan(0, $data);
     }
 
-    public function testUpdate()
-    {
-        $context = new Context();
-        $job = new UpdateJob($context);
-        $sid = 'demo.demo_sql_update1';
-        $data = [
-            'data' => [
-                'name' => "abcd",
-                'nick_name' => 'b',
-                'id_number' => '330323198888888888',
-                'gender' => 1
-            ],
-            'var' => ['name' => 'allen'],
-            'and' => [
-                ['gender', '=', 0],
-            ],
-            'and1' => [
-                ['id_number', '=', 0],
-            ],
 
-        ];
-        $options = [];
-        $job->setSid($sid)->setData($data)->setOptions($options);
-        $coroutine = $job->run();
 
-        $task = new Task($coroutine);
-        $task->run();
-        $result = $context->show();
-        $this->assertTrue($result['response']);
-    }
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+        public function testUpdate()
+        {
+            $context = new Context();
+            $job = new UpdateJob($context);
+            $sid = 'demo.demo_sql_update1';
+            $data = [
+                'data' => [
+                    'name' => "abcd",
+                    'nick_name' => 'b',
+                    'id_number' => '330323198888888888',
+                    'gender' => 1
+                ],
+                'var' => ['name' => 'allen'],
+                'and' => [
+                    ['gender', '=', 0],
+                ],
+                'and1' => [
+                    ['id_number', '=', 0],
+                ],
+
+            ];
+            $options = [];
+            $job->setSid($sid)->setData($data)->setOptions($options);
+            $coroutine = $job->run();
+
+            $task = new Task($coroutine);
+            $task->run();
+            $result = $context->show();
+            $this->assertTrue($result['response']);
+        }
+    */
     public function testSelectOne()
     {
         $context = new Context();
