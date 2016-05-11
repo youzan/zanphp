aa6954fb9c2ea390eaa34f67942c0b2c9b36d5ac
小牛 <xiaoniu@qima-inc.com>
2016-05-11 11:18:33 +0800

    connection

--- src/Network/Connection/ConnectionInitiator.php
+++ src/Network/Connection/ConnectionInitiator.php
@@ -57,27 +57,27 @@ class ConnectionInitiator
 
     private function initConfig($config)
     {
-        if (is_array($config)) {
-            foreach ($config as $k=>$cf) {
-                if (!isset($cf['engine'])) {
-                    if (is_array($config)) {
-                        $this->poolName = $this->poolName . '.' . $k;
-                        $this->initConfig($cf);
-                    }
-                } else {
-                    if (!isset($cf['pool']) || empty($cf['pool'])) {
-                        continue;
-                    }
-                    //创建连接池
-                    $this->poolName = $this->poolName . '.' . $k;
-                    $factoryType = $cf['engine'];
-                    if (in_array($factoryType, $this->engineMap)) {
-                        $factoryType = ucfirst($factoryType);
-                        $cf['pool']['pool_name'] = $this->directory . $this->poolName;
-                        $this->initPool($factoryType, $cf);
-                        $this->poolName='';
-                    }
-                }
+        if (!is_array($config)) {
+            return;
+        }
+        foreach ($config as $k=>$cf) {
+            if (!isset($cf['engine'])) {
+                $this->poolName = $this->poolName . '.' . $k;
+                $this->initConfig($cf);
+                continue;
+            }
+            if (!isset($cf['pool']) || empty($cf['pool'])) {
+                $this->poolName = '';
+                continue;
+            }
+            //创建连接池
+            $this->poolName = $this->poolName . '.' . $k;
+            $factoryType = $cf['engine'];
+            if (in_array($factoryType, $this->engineMap)) {
+                $factoryType = ucfirst($factoryType);
+                $cf['pool']['pool_name'] = $this->directory . $this->poolName;
+                $this->initPool($factoryType, $cf);
+                $this->poolName = '';
             }
         }
     }
