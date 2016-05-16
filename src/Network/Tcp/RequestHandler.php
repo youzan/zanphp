9eb7e8b3ae6a878d707a578cf05dcb15caf59a23
demon <demon@php.net>
2016-05-16 10:32:01 +0800

    tcp request timeout

--- src/Network/Tcp/RequestHandler.php
+++ src/Network/Tcp/RequestHandler.php
@@ -5,21 +5,30 @@ namespace Zan\Framework\Network\Tcp;
 use \swoole_server as SwooleServer;
 use Zan\Framework\Foundation\Core\Config;
 use Zan\Framework\Foundation\Core\Debug;
+use Zan\Framework\Foundation\Coroutine\Signal;
 use Zan\Framework\Foundation\Coroutine\Task;
+use Zan\Framework\Network\Server\Middleware\MiddlewareManager;
 use Zan\Framework\Network\Server\Monitor\Worker;
+use Zan\Framework\Network\Server\Timer\Timer;
 use Zan\Framework\Utilities\DesignPattern\Context;
 use Zan\Framework\Utilities\Types\Time;
 
 class RequestHandler {
     private $swooleServer = null;
     private $context = null;
+    private $response = null;
     private $fd = null;
     private $fromId = null;
+    private $task = null;
+    private $middleWareManager = null;
+
+    const DEFAULT_TIMEOUT = 30 * 1000;
 
 
     public function __construct()
     {
         $this->context = new Context();
+        $this->event = $this->context->getEvent();
     }
 
     public function handle(SwooleServer $swooleServer, $fd, $fromId, $data)
@@ -34,12 +43,13 @@ class RequestHandler {
     private function doRequest($data)
     {
         $request = new Request($this->fd, $this->fromId, $data);
-        $response = new Response($this->swooleServer, $request);
+        $response = $this->response = new Response($this->swooleServer, $request);
 
         $this->context->set('request_time', Time::stamp());
         $request_timeout = Config::get('server.request_timeout');
-        $request_timeout = $request_timeout ? $request_timeout : 30;
+        $request_timeout = $request_timeout ? $request_timeout : self::DEFAULT_TIMEOUT;
         $this->context->set('request_timeout', $request_timeout);
+        $this->context->set('request_end_event_name', $this->getRequestFinishJobId());
 
         try {
             $request->decode();
@@ -48,10 +58,17 @@ class RequestHandler {
                 Worker::instance()->reactionRelease();
                 return;
             }
-            
-            $requestTask = new RequestTask($request, $response, $this->context);
+
+            $this->middleWareManager = new MiddlewareManager($request, $this->context);
+            $requestTask = new RequestTask($request, $response, $this->context, $this->middleWareManager);
             $coroutine = $requestTask->run();
-            Task::execute($coroutine, $this->context);
+
+            //bind event
+            $this->event->once($this->getRequestFinishJobId(), [$this, 'handleRequestFinish']);
+            Timer::after($request_timeout, [$this, 'handleTimeout'], $this->getRequestTimeoutJobId());
+
+            $this->task = new Task($coroutine, $this->context);
+            $this->task->run();
         } catch(\Exception $e) {
             Worker::instance()->reactionRelease();
             //TODO: 格式化exception输出
@@ -59,8 +76,32 @@ class RequestHandler {
                 var_dump($e);
             }
             $response->sendException($e);
+            $this->event->fire($this->getRequestFinishJobId());
             return;
         }
     }
 
+    public function handleRequestFinish()
+    {
+        Timer::clearTickJob($this->getRequestTimeoutJobId());
+        $coroutine = $this->middleWareManager->executeTerminators($this->response);
+        Task::execute($coroutine, $this->context);
+    }
+    public function handleTimeout()
+    {
+        $this->task->setStatus(Signal::TASK_KILLED);
+        $e = new \Exception('server timeout');
+        $this->response->sendException($e);
+        $this->event->fire($this->getRequestFinishJobId());
+    }
+
+    private function getRequestFinishJobId()
+    {
+        return spl_object_hash($this) . '_request_finish';
+    }
+
+    private function getRequestTimeoutJobId()
+    {
+        return spl_object_hash($this) . '_handle_timeout';
+    }
 }
