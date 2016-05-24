c99beec9a13a5cb74143bdd9bb64d62385de4982
demon <demon@php.net>
2016-05-24 09:57:07 +0800

    tcp复用

--- src/Network/Connection/Pool.php
+++ src/Network/Connection/Pool.php
@@ -98,11 +98,16 @@ class Pool implements ConnectionPool
     
     public function recycle(Connection $conn)
     {
+        $evtName = null;
+        if ($this->freeConnection->isEmpty()) {
+            $evtName = $this->poolConfig['pool']['pool_name'] . '_free';
+        }
+        
         $this->freeConnection->push($conn);
         $this->activeConnection->remove($conn);
-        if (count($this->freeConnection) == 1) {
+        
+        if ($evtName) {
             //唤醒等待事件
-            $evtName = $this->poolConfig['pool']['pool_name'] . '_free';
             Event::fire($evtName, [], false);
         }
     }
