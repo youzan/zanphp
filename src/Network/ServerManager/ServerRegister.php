80148849794f3180ddab1ebda9ea487f7e69afcb
小牛 <xiaoniu@qima-inc.com>
2016-06-02 21:22:02 +0800

    server discovery

--- src/Network/ServerManager/ServerRegister.php
+++ src/Network/ServerManager/ServerRegister.php
@@ -7,18 +7,41 @@
  */
 namespace Zan\Framework\Network\ServerManager;
 
+use Zan\Framework\Foundation\Core\Env;
 use Zan\Framework\Network\Common\HttpClient;
+use Zan\Framework\Foundation\Core\Config;
 
 class ServerRegister
 {
-
-    public function register()
+    public function parseConfig($config)
     {
-        $httpClient = new HttpClient($host, $port);
-        yield $httpClient->post($uri, $params, $timeout);
+        $extData = [];
+        foreach ($config['services'] as $service) {
+            $extData[] = [
+                'service' => $service['service'],
+                'language' => 'php',
+                'timestamp' => time(),
+                'version' => '1.0.0',
+                'methods' => $service['methods'],
+            ];
+        }
+        return [
+            'Namespace' => 'com.youzan.service',
+            'SrvName' => $config['module'],
+            'IP' => Env::get('ip'),
+            'Port' => 8000,
+            'Protocol' => 'nova',
+            'Status' => 1,
+            'Weight' => 0,
+            'ExtData' => json_encode($extData),
+        ];
+    }
 
-
-
+    public function register($config)
+    {
+        $haunt = Config::get('haunt');
+        $httpClient = new HttpClient($haunt['register']['host'], $haunt['register']['port']);
+        yield $httpClient->post($haunt['register']['uri'], $this->parseConfig($config), $haunt['register']['timeout']);
     }
 
 
