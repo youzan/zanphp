9cfc9786ada2210d93c4820f0bb2ced6ff72d25a
卫星 <voidman.me@gmail.com>
2016-06-13 23:13:32 +0800

    增加注释

--- src/Utilities/Types/Json.php
+++ src/Utilities/Types/Json.php
@@ -4,6 +4,12 @@ namespace Zan\Framework\Utilities\Types;
 
 class Json
 {
+    /**
+     * @param mixed $value
+     * @param null $options
+     * @param int $depth
+     * @return string
+     */
     public static function encode($value, $options = null, $depth = 512)
     {
         if (is_null($options)) {
@@ -12,6 +18,13 @@ class Json
         return json_encode($value, $options, $depth);
     }
 
+    /**
+     * @param string $json
+     * @param bool $assoc
+     * @param int $depth
+     * @param int $options
+     * @return mixed
+     */
     public static function decode($json, $assoc = true, $depth = 512, $options = JSON_BIGINT_AS_STRING)
     {
         if (!is_string($json)) {
