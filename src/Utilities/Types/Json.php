6f8cbf9809f426b28f8f5ad5b4d665ce93057bd6
卫星 <voidman.me@gmail.com>
2016-06-13 23:08:15 +0800

    调整变量名称

--- src/Utilities/Types/Json.php
+++ src/Utilities/Types/Json.php
@@ -4,19 +4,19 @@ namespace Zan\Framework\Utilities\Types;
 
 class Json
 {
-    public static function encode($data, $options = null, $depth = 512)
+    public static function encode($value, $options = null, $depth = 512)
     {
         if (is_null($options)) {
             $options = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
         }
-        return json_encode($data, $options, $depth);
+        return json_encode($value, $options, $depth);
     }
 
-    public static function decode($data, $assoc = true, $depth = 512, $options = JSON_BIGINT_AS_STRING)
+    public static function decode($json, $assoc = true, $depth = 512, $options = JSON_BIGINT_AS_STRING)
     {
-        if (!is_string($data)) {
-            return $data;
+        if (!is_string($json)) {
+            return $json;
         }
-        return json_decode($data, $assoc, $depth, $options);
+        return json_decode($json, $assoc, $depth, $options);
     }
 }
