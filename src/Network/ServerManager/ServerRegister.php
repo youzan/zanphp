564cf1f2b161a3f8564807c3475cd27018360949
小牛 <xiaoniu@qima-inc.com>
2016-07-23 20:11:42 +0800

    postjson

--- src/Network/ServerManager/ServerRegister.php
+++ src/Network/ServerManager/ServerRegister.php
@@ -49,7 +49,7 @@ class ServerRegister
     {
         $haunt = Config::get('haunt');
         $httpClient = new HttpClient($haunt['register']['host'], $haunt['register']['port']);
-        $response = (yield $httpClient->post($haunt['register']['uri'], $this->parseConfig($config), null));
+        $response = (yield $httpClient->postJson($haunt['register']['uri'], $this->parseConfig($config), null));
         $register = $response->getBody();
 
         echo $register;
