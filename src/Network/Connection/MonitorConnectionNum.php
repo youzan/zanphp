61daa4154705f85ace0df3c33f1a897e61402705
xiaolong <xiaolong@qima-inc.com>
2016-07-25 11:26:44 +0800

    reduce-time

--- src/Network/Connection/MonitorConnectionNum.php
+++ src/Network/Connection/MonitorConnectionNum.php
@@ -37,13 +37,20 @@ class MonitorConnectionNum {
             $activeNum = $pool->getActiveConnection()->length();
             $freeNum = $pool->getFreeConnection()->length();
             $sumNum = $activeNum + $freeNum;
-            if ($sumNum <=0 || $freeNum*4 < $sumNum) {
+            if ($sumNum <=0) {
                 continue;
             }
-            for ($i=0; $i<$freeNum; $i++) {
+            $poolConfig = Config::get('connection.' . $poolKey)['pool'];
+            $minConnectionNum = isset($poolConfig['minimum-connection-count']) ? $poolConfig['minimum-connection-count'] : 1;
+            if ($freeNum <= $minConnectionNum) {
+                continue;
+            }
+            for ($i=0; $i<$freeNum-$minConnectionNum; $i++) {
                 $conn = $pool->getFreeConnection()->pop();
                 if ($conn->lastUsedTime == 0 || (Time::current(true) - $conn->lastUsedTime) > $timeInterval/1000) {
                     $conn->closeSocket();
+                } else {
+                    $pool->getFreeConnection()->push($conn);
                 }
             }
         }
