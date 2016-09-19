1c0746aa492d4385c0489b3b63fee74474d8a670
小牛 <xiaoniu@qima-inc.com>
2016-09-19 17:45:40 +0800

    context

--- src/Network/Server/Middleware/MiddlewareConfig.php
+++ src/Network/Server/Middleware/MiddlewareConfig.php
@@ -96,7 +96,7 @@ class MiddlewareConfig
             KVTerminator::class,
 
             TraceTerminator::class,
-            ClearContextTerminator::class,
+//            ClearContextTerminator::class,
         ];
         return array_merge($terminators, $this->zanTerminators, $baseTerminators);
     }
