c6e214dd9e450edca67c627538d68bb419d55ae4
小牛 <xiaoniu@qima-inc.com>
2016-10-19 10:12:58 +0800

    config

--- src/Network/Common/ApiConfig.php
+++ src/Network/Common/ApiConfig.php
@@ -607,7 +607,7 @@ return [
         ],
         'trade' => [
             'type' => 'local',
-            'host' => 'http://trade.api.youzan.com/',
+            'host' => 'http://api.koudaitong.com',
             'sub' => [
                 [
                     'mod' => 'trade.buy.bookByParams',
@@ -1075,7 +1075,7 @@ return [
         ],
         'trade' => [
             'type' => 'local',
-            'host' => 'http://trade.api.youzan.com/',
+            'host' => 'http://api.koudaitong.com',
             'sub' => [
                 [
                     'mod' => 'trade.buy.bookByParams',
