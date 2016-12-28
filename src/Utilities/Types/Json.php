6ea7198484bd122af22fc2b8cd35c0ee4e3b9629
chuxiaofeng <chuxiaofeng@youzan.com>
2016-12-28 21:40:54 +0800

    fix json::encode

--- src/Utilities/Types/Json.php
+++ src/Utilities/Types/Json.php
@@ -2,20 +2,37 @@
 
 namespace Zan\Framework\Utilities\Types;
 
+use Zan\Framework\Foundation\Exception\System\InvalidArgumentException;
+
 class Json
 {
     /**
      * @param mixed $value
-     * @param null $options
+     * @param int $options
      * @param int $depth
      * @return string
+     * @throws InvalidArgumentException
      */
-    public static function encode($value, $options = null, $depth = 512)
+    public static function encode($value, $options = 0, $depth = 512)
     {
-        if (is_null($options)) {
-            $options = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
+        $options = $options | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRESERVE_ZERO_FRACTION;
+        $data = json_encode($value, $options, $depth);
+
+        if ($data === false) {
+            $errno = json_last_error();
+
+            if ($errno === JSON_ERROR_UTF8) {
+                $data = json_encode(static::cleanUtf8($value), $options, $depth);
+                if ($data === false) {
+                    goto ex;
+                }
+            } else {
+                ex:
+                throw new InvalidArgumentException("JSON encoding failed: " . json_last_error_msg(), $errno, null, func_get_args());
+            }
         }
-        return json_encode($value, $options, $depth);
+
+        return $data;
     }
 
     /**
@@ -24,12 +41,47 @@ class Json
      * @param int $depth
      * @param int $options
      * @return mixed
+     * @throws InvalidArgumentException
      */
     public static function decode($json, $assoc = true, $depth = 512, $options = JSON_BIGINT_AS_STRING)
     {
         if (!is_string($json)) {
             return $json;
         }
-        return json_decode($json, $assoc, $depth, $options);
+
+        $data = json_decode($json, $assoc, $depth, $options);
+
+        $errno = json_last_error();
+        if ($errno !== JSON_ERROR_NONE) {
+            throw new InvalidArgumentException("JSON decode failed: " . json_last_error_msg(), $errno, null, func_get_args());
+        }
+
+        return $data;
+    }
+
+    /**
+     * 清除非utf8字符串, 处理Thrift异常信息json_encode编码失败
+     * @param $value
+     * @return array
+     */
+    private static function cleanUtf8($value)
+    {
+        $cleanScalar = function(&$string) {
+            if (is_string($string)) {
+                $isUtf8 = preg_match('//u', $string);
+                if (!$isUtf8) {
+                    $sanitize = function ($m) { return utf8_encode($m[0]); };
+                    $string = preg_replace_callback('/[\x80-\xFF]+/', $sanitize, $string);
+                }
+            }
+        };
+
+        if (is_string($value)) {
+            $cleanScalar($value);
+        } else if (is_array($value)) {
+            array_walk_recursive($value, $cleanScalar);
+        }
+
+        return $value;
     }
 }
