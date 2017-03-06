c52fae4b07674b0a318abdb7b45096d30aa0160b
xiaohengjin <xiaohengjin@youzan.com>
2017-03-06 15:15:05 +0800

    add dns client test case

---
+++ test/Network/Http/DnsClientTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marsnowxiao
+ * Date: 2017/3/6
+ * Time: 下午3:10
+ */
+
+use Zan\Framework\Test\Foundation\Coroutine\TaskTest;
+use Zan\Framework\Network\Common\DnsClient;
+
+class DnsClientTest extends TaskTest {
+    public function testAccessableHost()
+    {
+        DnsClient::lookup("www.baidu.com", function ($host, $ip) {
+            var_dump(func_get_args());
+        });
+    }
+    public function testUnAccessableHost()
+    {
+        DnsClient::lookup("unreachable", function ($host, $ip) {
+            var_dump(func_get_args());
+        },
+            function () {
+                var_dump(func_get_args());
+            });
+    }
+}
