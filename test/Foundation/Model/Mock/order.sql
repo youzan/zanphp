1dc22d1db9655bc6e96ae213c7eb33a8e60d0182
chuxiaofeng <chuxiaofeng@youzan.com>
2017-06-02 20:27:45 +0800

    暂时忽略test

--- test/Foundation/Model/Mock/order.sql
+++
@@ -1,7 +0,0 @@
-
--- table order
-create table order(
-  id int(10) unsigned not null default 0 comment 'id',
-  orderNo varchar(32) not null default '' comment '订单号',
-  customerName varchar(50) not null default '' comment ''
-)engine=innodb;
--- test/resource/config/test/connection/kvstore.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: marsnowxiao
- * Date: 2017/3/28
- * Time: 下午4:46
- */
-return [];
