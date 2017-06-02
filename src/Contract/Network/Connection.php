22e40f57c9b8a61d696f6d169fe577be9632a411
chuxiaofeng <chuxiaofeng@youzan.com>
2017-06-02 15:38:53 +0800

    整理代码

--- src/Contract/Network/Connection.php
+++ src/Contract/Network/Connection.php
@@ -14,7 +14,6 @@ interface Connection
     public function getSocket();
     public function release();
     public function close();
-    
     public function getEngine();
     public function getConfig();
     public function heartbeat();
